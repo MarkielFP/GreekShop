513593a044f8506bea5e942032ef2d4da4046691
Markiel <mgluchowski89@gmail.com>
2018-09-16 13:03:56 +0200

    initial commit

---
+++ src/main/java/com/greekshop/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.greekshop;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GreekshopApplication.class);
+	}
+
+}
